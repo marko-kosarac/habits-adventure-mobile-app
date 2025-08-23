4b887cb49dc48bc4ed88a511ef1ca532200c2c23
Marko <marko_kosarac@hotmail.com>
2025-08-23 17:17:40 +0200

    UI for user profile created

--- app/src/main/java/com/example/mobilnaaplikacija/fragments/HomePageFragment.java
+++ app/src/main/java/com/example/mobilnaaplikacija/fragments/HomePageFragment.java
@@ -2,11 +2,18 @@ package com.example.mobilnaaplikacija.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.mobilnaaplikacija.R;
 
@@ -15,11 +22,22 @@ public class HomePageFragment extends Fragment {
     public HomePageFragment() {
         // Required empty public constructor
     }
-
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_page, container, false);
+
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button buttonProfile = view.findViewById(R.id.buttonProfile);
+        buttonProfile.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.action_homePageFragment_to_userProfileFragment);
+        });
     }
 }
