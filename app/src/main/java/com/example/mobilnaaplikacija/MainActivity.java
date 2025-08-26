a3b776932c40ecc9598fd0a822f0b16e1ba1e86d
Marko <marko_kosarac@hotmail.com>
2025-08-26 11:57:13 +0200

    Minor bug fix

--- app/src/main/java/com/example/mobilnaaplikacija/MainActivity.java
+++ app/src/main/java/com/example/mobilnaaplikacija/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mobilnaaplikacija;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -87,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
             drawer.closeDrawers();
             return true;
         });
+        hideSystemUI();
     }
 
     public void onLoginSuccess() {
@@ -125,5 +127,16 @@ public class MainActivity extends AppCompatActivity {
         navView.getMenu().clear();
         navView.inflateMenu(R.menu.main_drawer);
 
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
