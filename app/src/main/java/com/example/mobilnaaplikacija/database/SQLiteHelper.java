18784f2a47d7dd3126b4e2059d29e0b08e51a1e8
mareenah <kicanovicmarina@gmail.com>
2025-09-09 22:19:56 +0200

    SQLiteHelper updated for tasks

--- app/src/main/java/com/example/mobilnaaplikacija/database/SQLiteHelper.java
+++ app/src/main/java/com/example/mobilnaaplikacija/database/SQLiteHelper.java
@@ -7,6 +7,7 @@ import android.util.Log;
 
 public class SQLiteHelper extends SQLiteOpenHelper {
 
+    //Equipment table
     public static final String TABLE_EQUIPMENT = "EQUIPMENT";
     public static final String COLUMN_ID = "_id";
     public static final String COLUMN_NAME = "name";
@@ -16,8 +17,26 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     public static final String COLUMN_DURATION = "duration";
     public static final String COLUMN_PRICE = "price";
 
-    private static final String DATABASE_NAME = "equipment.db";
-    private static final int DATABASE_VERSION = 4;
+    // Tasks table
+    public static final String TABLE_TASKS = "TASKS";
+    public static final String COLUMN_TASK_ID = "task_id";
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_TASK_NAME = "task_name";
+    public static final String COLUMN_TASK_DESCRIPTION = "task_description";
+    public static final String COLUMN_CATEGORY = "category";
+    public static final String COLUMN_FREQUENCY = "frequency";
+    public static final String COLUMN_START_DATE = "start_date";
+    public static final String COLUMN_END_DATE = "end_date";
+    public static final String COLUMN_TIME = "time";
+    public static final String COLUMN_IS_WHOLE_DAY = "is_whole_day";
+    public static final String COLUMN_INTERVAL = "interval";
+    public static final String COLUMN_UNIT = "unit";
+    public static final String COLUMN_DIFFICULTY = "difficulty";
+    public static final String COLUMN_IMPORTANCE = "importance";
+    public static final String COLUMN_STATUS = "status";
+
+    private static final String DATABASE_NAME = "appdata.db";
+    private static final int DATABASE_VERSION = 5;
 
     private static final String DB_CREATE = "CREATE TABLE " + TABLE_EQUIPMENT + " ("
             + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -29,20 +48,41 @@ public class SQLiteHelper extends SQLiteOpenHelper {
             + COLUMN_PRICE + " INTEGER"
             + ");";
 
+    private static final String DB_CREATE_TASKS =
+            "CREATE TABLE " + TABLE_TASKS + " (" +
+                    COLUMN_TASK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_USER_ID + " TEXT, " +
+                    COLUMN_TASK_NAME + " TEXT, " +
+                    COLUMN_TASK_DESCRIPTION + " TEXT, " +
+                    COLUMN_CATEGORY + " TEXT, " +
+                    COLUMN_FREQUENCY + " TEXT, " +
+                    COLUMN_START_DATE + " TEXT, " +
+                    COLUMN_END_DATE + " TEXT, " +
+                    COLUMN_TIME + " TEXT, " +
+                    COLUMN_IS_WHOLE_DAY + " INTEGER, " +
+                    COLUMN_INTERVAL + " INTEGER, " +
+                    COLUMN_UNIT + " TEXT, " +
+                    COLUMN_DIFFICULTY + " TEXT, " +
+                    COLUMN_IMPORTANCE + " TEXT, " +
+                    COLUMN_STATUS + " TEXT" +
+                    ");";
+
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        Log.i("EQ_DB", "Creating Equipment table");
+        Log.i("EQ_DB", "Creating Equipment and Tasks table");
         db.execSQL(DB_CREATE);
+        db.execSQL(DB_CREATE_TASKS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.i("EQ_DB", "Upgrading Equipment table from " + oldVersion + " to " + newVersion);
+        Log.i("EQ_DB", "Upgrading tables from " + oldVersion + " to " + newVersion);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_EQUIPMENT);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASKS);
         onCreate(db);
     }
 }
