16c177fdde254773f18a2aef3536c73d0e9ca03e
mareenah <kicanovicmarina@gmail.com>
2025-09-17 12:20:38 +0200

    Add task button removed from home page

--- app/src/main/java/com/example/mobilnaaplikacija/fragments/HomePageFragment.java
+++ app/src/main/java/com/example/mobilnaaplikacija/fragments/HomePageFragment.java
@@ -1,6 +1,5 @@
 package com.example.mobilnaaplikacija.fragments;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +7,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import com.example.mobilnaaplikacija.databinding.FragmentHomePageBinding;
 
 public class HomePageFragment extends Fragment {
@@ -17,7 +17,6 @@ public class HomePageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        Log.i("MoblinaAplikacija", "onCreateView Home Page Fragment");
         binding = FragmentHomePageBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -25,14 +24,6 @@ public class HomePageFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
-
-        binding.btnAddTask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new AddTaskFragment().show(
-                        getChildFragmentManager(), "New task");
-            }
-        });
     }
 
     @Override
