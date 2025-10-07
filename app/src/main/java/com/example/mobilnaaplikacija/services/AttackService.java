a7e7ef19a036187303e614f80906ae77b1720c5e
mareenah <kicanovicmarina@gmail.com>
2025-10-07 23:11:26 +0200

    Added attack service

--- app/src/main/java/com/example/mobilnaaplikacija/services/AttackService.java
+++ app/src/main/java/com/example/mobilnaaplikacija/services/AttackService.java
@@ -1,6 +1,7 @@
 package com.example.mobilnaaplikacija.services;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.example.mobilnaaplikacija.database.SQLiteHelper;
 import com.example.mobilnaaplikacija.model.Attack;
@@ -10,11 +11,17 @@ import java.util.List;
 
 public class AttackService {
     private AttackRepository attackRepository;
+    private TaskService taskService;
     public AttackService(Context context) {
         this.attackRepository = new AttackRepository(new SQLiteHelper(context));
+        this.taskService = new TaskService(context);
     }
 
     public List<Attack> getAttacksByUser (String userId) {
         return attackRepository.getAttacksByUser(userId);
+    }
+
+    public Attack add (Attack attack) {
+        return attackRepository.add(attack);
     }
 }
