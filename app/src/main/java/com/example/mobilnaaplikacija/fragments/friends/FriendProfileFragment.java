5353c2ffa54780b7c82183de60b106140a46cceb
mareenah <kicanovicmarina@gmail.com>
2025-10-16 23:38:13 +0200

    Removed old code for quota and fixed null prevention when setting count

--- app/src/main/java/com/example/mobilnaaplikacija/fragments/friends/FriendProfileFragment.java
+++ app/src/main/java/com/example/mobilnaaplikacija/fragments/friends/FriendProfileFragment.java
@@ -201,7 +201,7 @@ public class FriendProfileFragment extends Fragment {
         eq.setPrice(((Number) data.get("price")).intValue());
         eq.setQuantity(((Number) data.get("quantity")).intValue());
         eq.setActive((Boolean) data.get("active"));
-        eq.setCount(((Number) data.get("count")).intValue());
+        eq.setCount(data.get("count") != null ? ((Number) data.get("count")).intValue() : 0);
         eq.setType(Equipment.Type.valueOf((String) data.get("type")));
         return eq;
     }
